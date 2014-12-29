df35cae61bb96dd5bd69f77b4e7b15cbbfb89f74
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2014-12-29 14:16:58 +0300

    add new module js.serializer

---
+++ js/js.serializer/src/org/jetbrains/kotlin/utils/serializer/Serializer.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2010-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.kotlin.utils.serializer
+
+import java.io.ByteArrayOutputStream
+import java.io.File
+import org.jetbrains.kotlin.builtins.BuiltInsSerializationUtil
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.ModuleDescriptor
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.JetFile
+import org.jetbrains.kotlin.resolve.descriptorUtil.classId
+import org.jetbrains.kotlin.resolve.DescriptorUtils
+import org.jetbrains.kotlin.resolve.scopes.DescriptorKindFilter
+import org.jetbrains.kotlin.serialization.builtins.BuiltInsSerializerExtension
+import org.jetbrains.kotlin.serialization.DescriptorSerializer
+import org.jetbrains.kotlin.serialization.NameSerializationUtil
+import org.jetbrains.kotlin.serialization.ProtoBuf
+import org.jetbrains.kotlin.utils.LibraryUtils
+
+public class Serializer() {
+
+    public fun serialize(moduleName: String, moduleDescriptor: ModuleDescriptor, files: List<JetFile>, metaFile: File) {
+        serialize(moduleDescriptor, files) {
+            (packageSet, content) ->
+            LibraryUtils.writeMetadata(moduleName, packageSet, content, metaFile)
+        }
+    }
+
+    public fun serialize(moduleDescriptor: ModuleDescriptor, files: List<JetFile>, saveFun: (Set<String>, Map<String, ByteArray>) -> Unit) {
+        val packageSet = hashSetOf<String>()
+        val map = hashMapOf<String, ByteArray>()
+        serialize(moduleDescriptor, files, { packageSet.add(it.asString()) }) {
+            (fileName, stream) -> map[fileName] = stream.toByteArray()
+        }
+        saveFun(packageSet, map)
+    }
+
+    public fun serialize(
+            moduleDescriptor: ModuleDescriptor,
+            files: List<JetFile>,
+            packageFun: (FqName) -> Unit,
+            writeFun: (String, ByteArrayOutputStream) -> Unit
+    ) {
+        val packageSet = hashSetOf<FqName>()
+        for( file in files) {
+            var fqName = file.getPackageFqName()
+            packageSet.add(fqName)
+            while(!fqName.isRoot()) {
+                packageSet.add(fqName)
+                fqName = fqName.parent()
+            }
+        }
+
+        packageSet.forEach { fqName ->
+            packageFun(fqName)
+            serializePackage(moduleDescriptor, fqName, writeFun)
+        }
+    }
+
+    fun serializePackage(module: ModuleDescriptor, fqName: FqName, writeFun: (String, ByteArrayOutputStream) -> Unit) {
+        val packageView = module.getPackage(fqName) ?: error("No package resolved in $module")
+
+        // TODO: perform some kind of validation? At the moment not possible because DescriptorValidator is in compiler-tests
+        // DescriptorValidator.validate(packageView)
+
+        val skip: (DeclarationDescriptor) -> Boolean = { DescriptorUtils.getContainingModule(it) != module} //{ isBuiltin(it) }
+
+        val serializer = DescriptorSerializer.createTopLevel(BuiltInsSerializerExtension)
+
+        val classifierDescriptors = DescriptorSerializer.sort(packageView.getMemberScope().getDescriptors(DescriptorKindFilter.CLASSIFIERS))
+
+        ClassSerializationUtil.serializeClasses(classifierDescriptors, serializer, object : ClassSerializationUtil.Sink {
+            override fun writeClass(classDescriptor: ClassDescriptor, classProto: ProtoBuf.Class) {
+                val stream = ByteArrayOutputStream()
+                classProto.writeTo(stream)
+                writeFun(getFileName(classDescriptor), stream)
+            }
+        }, skip)
+
+        val packageStream = ByteArrayOutputStream()
+        val fragments = module.getPackageFragmentProvider().getPackageFragments(fqName)
+        val packageProto = serializer.packageProto(fragments, skip).build() ?: error("Package fragments not serialized: $fragments")
+        packageProto.writeTo(packageStream)
+        writeFun(BuiltInsSerializationUtil.getPackageFilePath(fqName), packageStream)
+
+        val nameStream = ByteArrayOutputStream()
+        NameSerializationUtil.serializeStringTable(nameStream, serializer.getStringTable())
+        writeFun(BuiltInsSerializationUtil.getStringTableFilePath(fqName), nameStream)
+    }
+
+    fun getFileName(classDescriptor: ClassDescriptor): String {
+        return BuiltInsSerializationUtil.getClassMetadataPath(classDescriptor.classId)
+    }
+}
