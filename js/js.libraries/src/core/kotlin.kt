4e992acf28a74f4e57677605bdcfef6f97e831d8
Michael Nedzelsky <Michael.Nedzelsky@jetbrains.com>
2015-02-16 20:14:59 +0300

    JS backend: add intrinsic annotations for array factory functions

--- js/js.libraries/src/core/kotlin.kt
+++ js/js.libraries/src/core/kotlin.kt
@@ -1,34 +1,35 @@
 package kotlin
 
 import java.util.*
+import kotlin.internal.Intrinsic
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun <T> array(vararg value : T): Array<T> = noImpl
 
 // "constructors" for primitive types array
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun doubleArray(vararg content : Double): DoubleArray    = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun floatArray(vararg content : Float): FloatArray       = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun longArray(vararg content : Long): LongArray          = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun intArray(vararg content : Int): IntArray             = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun charArray(vararg content : Char): CharArray          = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun shortArray(vararg content : Short): ShortArray       = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun byteArray(vararg content : Byte): ByteArray          = noImpl
 
-library
+library [Intrinsic("kotlin.arrays.array")]
 public fun booleanArray(vararg content : Boolean): BooleanArray = noImpl
 
 library("copyToArray")
