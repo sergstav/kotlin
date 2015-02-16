92a577bd6885b57b332e9c9b0c1f9bf288ebb4aa
Pavel V. Talanov <talanov.pavel@gmail.com>
2015-02-16 17:45:23 +0300

    Can't omit default object name in imports and types

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/Importer.kt
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/Importer.kt
@@ -37,11 +37,6 @@ public class Importer {
         else if (descriptor is ClassDescriptor && descriptor.getKind() != ClassKind.OBJECT) {
             allUnderImportScopes.add(descriptor.getStaticScope())
             allUnderImportScopes.add(descriptor.getUnsubstitutedInnerClassesScope())
-
-            val classObjectDescriptor = descriptor.getDefaultObjectDescriptor()
-            if (classObjectDescriptor != null) {
-                allUnderImportScopes.add(classObjectDescriptor.getUnsubstitutedInnerClassesScope())
-            }
         }
     }
 
